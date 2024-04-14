3863425520cef20e77d81fa446c00545001cf31c
juanfra312003 <juanframireze@javeriana.edu.co>
2024-04-14 14:51:55 -0500

    Feature: Entidad PTXS

---
+++ PackMyTripBackEnd/PackMyTripBackEnd/Entidades/PaqueteTuristicoXServicio.cs
@@ -0,0 +1,10 @@
+namespace PackMyTripBackEnd.Entidades
+{
+    public class PaqueteTuristicoXServicio
+    {
+        public int id { get; set; }
+        public int idPaquete { get; set; }
+        public int idServicio { get; set; }
+        public TimeSpan tiempoEstancia { get; set; }
+    }
+}
